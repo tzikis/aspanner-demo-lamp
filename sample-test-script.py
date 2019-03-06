f6187fe472083340d0624e760d642c7cc2ec0aa7
Vasilis Georgitzikis <tzikis@gmail.com>
2019-03-06 16:28:04 +0200

    erw

--- sample-test-script.py
+++ sample-test-script.py
@@ -34,7 +34,7 @@ def turn_ap_on():
 
 def turn_ap_off():
     print("#### Setting WiFi Access Point Off ####")
-    send_raspberry_command("wifioff")
+    return_val = send_raspberry_command("wifioff")
     if(return_val != True):
         sys.exit(1)
     print("####    Access Point Set to Off    ####")
